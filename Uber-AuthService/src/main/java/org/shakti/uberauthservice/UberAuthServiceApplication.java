f2a18afd26f0363a3a6db4cda9fa3d51c7112f0b
shakti <kumarshakti2017@gmail.com>
2025-06-16 09:08:16 +0530

    initial commit

---
+++ Uber-AuthService/src/main/java/org/shakti/uberauthservice/UberAuthServiceApplication.java
@@ -0,0 +1,14 @@
+package org.shakti.uberauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthServiceApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(UberAuthServiceApplication.class, args);
+    }
+
+}
---
+++ Uber-AuthService/src/test/java/org/shakti/uberauthservice/UberAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shakti.uberauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
