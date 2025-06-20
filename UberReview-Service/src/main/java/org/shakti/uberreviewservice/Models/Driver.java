bc78fa8369cfff37497c8928bf1d1dabf14815f3
shakti <kumarshakti2017@gmail.com>
2025-06-20 11:58:51 +0530

    added updated snapshot of commonlibraries

--- UberReview-Service/src/main/java/org/shakti/uberreviewservice/Models/Driver.java
+++ UberReview-Service/src/main/java/org/shakti/uberreviewservice/Models/Driver.java
@@ -1,21 +1,20 @@
 package org.shakti.uberreviewservice.Models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Entity
-@Getter
 @Setter
 @Builder
 @NoArgsConstructor
+@Getter
 @AllArgsConstructor
 public class Driver extends BaseModel{
     private String name;
