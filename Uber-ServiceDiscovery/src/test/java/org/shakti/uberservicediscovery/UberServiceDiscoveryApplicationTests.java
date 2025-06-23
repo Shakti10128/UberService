dabcad13077ea3590bb33f140b31c988fe9c90b8
shakti <kumarshakti2017@gmail.com>
2025-06-23 10:11:37 +0530

    initial commit with basic configs

---
+++ Uber-ServiceDiscovery/src/test/java/org/shakti/uberservicediscovery/UberServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shakti.uberservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
